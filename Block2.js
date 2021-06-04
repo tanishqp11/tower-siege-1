8eb80c21606dc373477bc2d01c3d311fda2dfe65
tanishqp11 <78473065+tanishqp11@users.noreply.github.com>
2021-06-04 09:04:57 +0530

    Add files via upload

---
+++ Block2.js
@@ -0,0 +1,29 @@
+class Block2{
+    constructor(x,y,width,height){
+        var options = {
+
+            restituition: 0.4,
+            friction:0.0
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world, this.body);
+
+    }
+
+    display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        stroke(0)
+        fill(255,192,203);
+        strokeWeight(1.5);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+        
+    }
+}
---
+++ Block4.js
@@ -0,0 +1,29 @@
+class Block4{
+    constructor(x,y,width,height){
+        var options = {
+
+            restituition: 0.4,
+            friction:0.0
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world, this.body);
+
+    }
+
+    display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        stroke(0)
+        fill(128,128,128);
+        strokeWeight(1.5);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+        
+    }
+}
